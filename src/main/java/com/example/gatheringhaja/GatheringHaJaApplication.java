3fe05dab7b6e59cc39102a9c2d1dfcfcdb3be48e
Woogie95 <choisw950928@gmail.com>
2023-12-13 17:25:59 +0900

    Initial commit

---
+++ src/main/java/com/example/gatheringhaja/GatheringHaJaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gatheringhaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatheringHaJaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatheringHaJaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gatheringhaja/GatheringHaJaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gatheringhaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatheringHaJaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
